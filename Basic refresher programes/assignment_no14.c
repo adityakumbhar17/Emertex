e07da8e45e1471b1e5f240c632795663dfea4888
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-01 18:54:49 +0530

    Update assignment_no14.c

--- Basic refresher programes/assignment_no14.c
+++ Basic refresher programes/assignment_no14.c
@@ -1,1 +1,36 @@
+/*
+check the number is prime or not
+*/
+#include <stdio.h>
 
+int main()
+{
+    //Write logic to check the given number is prime or not
+    int num;
+   // printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num<0)
+    {
+        printf("Invalid input");
+    }
+    else
+    {
+    int flag=0;
+    for(int i=2;i*i<=num;i++)
+    {
+     if(num%i==0)
+     {
+         flag=1;
+         break;
+     }
+    }
+    if(flag==0)
+    {
+        printf("%d is a prime number",num);
+    }
+    else
+    {
+        printf("%d is not a prime number",num);   
+    }
+    }
+}
