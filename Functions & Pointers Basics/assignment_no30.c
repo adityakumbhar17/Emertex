d29ede5cb0835f10dc2464bec66242af5affda1d
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-04 19:10:07 +0530

    WAP to find factorial of given number using recursion

--- Functions & Pointers Basics/assignment_no30.c
+++ Functions & Pointers Basics/assignment_no30.c
@@ -1,1 +1,35 @@
-
+/* 
+Name- Aditya shivaji kumbhar
+Date-22-08-2024;
+description- WAP to find factorial of given number using recursion
+Sample Execution:
+Enter the value of N : 7
+Factorial of the given number is 5040
+*/
+#include<stdio.h>
+int factonum(int n)
+{
+    //base case
+    if(n==0)
+    {
+        return 1;
+    }
+    return n*factonum(n-1);
+}
+int main()
+{
+    int num;
+   // printf("Enter the value of N : ");
+    scanf("%d",&num);
+    if(num<0)
+    {
+        printf("Invalid Input");
+    }
+    else
+    {
+    int ans=factonum(num);
+    printf("Factorial of the given number is %d",ans);
+    return 0;
+    }
+    
+}
