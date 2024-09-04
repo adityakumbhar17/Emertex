c01b82edda1b1e5f4a7765866181f99d4db99d9a
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-04 19:11:58 +0530

    WAP to generate fibbonacci numbers using recursion

--- Functions & Pointers Basics/assignment_no31.c
+++ Functions & Pointers Basics/assignment_no31.c
@@ -1,1 +1,43 @@
-
+/*
+Name- Aditya shivaji kumbhar
+Description-WAP to generate fibbonacci numbers using recursion
+Date -21-8-2024
+Sample input-Enter a number: 8
+Sample output- 0, 1, 1, 2, 3, 5, 8
+*/
+#include<stdio.h>
+int fibonum (int n)
+{
+    //base case 
+    if(n==0)
+    {
+        return 0;
+    }
+    if(n==1)
+    {
+        return 1;
+    }
+    //recrsive function
+    return fibonum(n-1)+fibonum (n-2);
+}
+int main()
+{
+    
+    int n,nextnum=0;
+    //printf("Enter the number : ");
+    scanf("%d",&n);
+    if(n<0)
+    {
+        printf(" Invalid input");
+    }
+    for(int i=0;nextnum<=n;i++)
+    {
+        nextnum=fibonum(i);
+        if(nextnum>n)
+        {
+            break;
+        }
+        printf("%d,",nextnum);
+    }
+    
+}
