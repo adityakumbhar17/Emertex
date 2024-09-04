c6f7834f34e8e9a021306cd4a5c4d01858606d36
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-04 18:46:22 +0530

    WAP to implement your own ispunct() function

--- Functions & Pointers Basics/assignment_no25.c
+++ Functions & Pointers Basics/assignment_no25.c
@@ -1,1 +1,39 @@
+/*
+Name - Aditya shivaji kumbhar
+description-WAP to implement your own ispunct() function
+date-07-08--2024
+*/
+#include <stdio.h>
 
+int my_ispunct(int);
+
+int main()
+{
+    char ch;
+    int ret;
+    
+    printf("Enter the character:");
+    scanf("%c", &ch);
+    
+    ret = my_ispunct(ch);
+    if(ret==1)
+    {
+       printf("Entered character is punctuation character"); 
+    }
+    else
+    {
+        printf("Entered character is not punctuation character");
+    }
+   
+}
+int my_ispunct(int ch)
+{
+    if(ch>=33 && ch<= 47 || ch>=58 && ch<=63)
+    {
+     return 1;
+    }
+    else
+    {
+     return 0;
+    }
+}
