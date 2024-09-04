8ad417cea2dc837b1508fc0dfed5f0cdc9442eea
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-04 19:05:40 +0530

    WAP to implement your own isxdigit() function

--- Functions & Pointers Basics/assignment_no26.c
+++ Functions & Pointers Basics/assignment_no26.c
@@ -1,1 +1,44 @@
+/* 
+Name- Aditya Shivaji kumbhaar 
+description- WAP to implement your own isxdigit() function
+Date- 07-08-2024
+*/
+#include <stdio.h>
+
+int is_xdigit(int);
+
+int main()
+{
+    char ch;
+    short ret;
+    
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    
+    ret = is_xdigit(ch);
+    
+   if(ret==1)
+   {
+       printf("Entered character is an hexadecimal digit");
+   }
+   else
+   {
+      printf("Entered character is not an hexadecimal digit"); 
+   }
+    
+    return 0;
+}
+
+int is_xdigit(int n)
+{
+    if(n>='0' && n<='9' || n>='a' && n<='f'|| n>='A' && n<='F')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 
