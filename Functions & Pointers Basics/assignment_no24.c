3b14daef0e626e8cf20a6a772a1f4028eb36c67b
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-04 18:45:07 +0530

    WAP to implement your own islower() function

--- Functions & Pointers Basics/assignment_no24.c
+++ Functions & Pointers Basics/assignment_no24.c
@@ -1,1 +1,42 @@
+/* 
+Name- Aditya shivaji kumbhar
+description- WAP toimpliment your own islower()function
+Date-07-08-2024
+*/
 
+#include <stdio.h>
+
+int my_islower(int);
+
+int main()
+{
+    char ch;
+    int ret;
+    
+    printf("Enter the character:");
+    scanf("%c", &ch);
+    
+    ret = my_islower(ch);
+    /*
+        Based on return value, print whether ch is lower case alphabet or not
+    */
+    if(ret==1)
+    {
+        printf("Entered character is lower case alphabet");
+    }
+    else 
+    {
+       printf("Entered character is not lower case alphabet"); 
+    }
+}
+int my_islower(int ch)
+{
+    if(ch>=97 && ch<=122)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
