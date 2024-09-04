b6bea2f28cfb65cfb11b1b7d4bc7e29d19939932
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-04 18:32:10 +0530

    Update assignment_no15.c

--- Functions & Pointers Basics/assignment_no15.c
+++ Functions & Pointers Basics/assignment_no15.c
@@ -1,1 +1,32 @@
+/* 
+Name -Aditya shivaji Kumbhar 
+description-
+If num is 10 and n is 2,
+10 -> 0 0 0 0 1 0 1 0 
+Take 2 bits from LSB end of 10 (1 0) and return the corresponding decimal of that. 
+So get_nbits(10, 2) function should return 2
+Date- 04-09-2024
+sample execution-
+Enter the number: 10
+Enter number of bits: 3
+Result = 2
+*/
 
+#include <stdio.h>
+
+int get_nbits(int num, int n)
+{
+    return num&((1<<n)-1);
+}
+
+int main()
+{
+    int num, n, res = 0;
+    
+    printf("Enter num and n:");
+    scanf("%d%d", &num, &n);
+    
+    res = get_nbits(num, n);
+    
+    printf("Result = %d\n", res);
+}
