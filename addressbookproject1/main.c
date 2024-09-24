aba9a63b2c572423fc0aa475a084b0083e882530
adityakumbhar17 <163285310+adityakumbhar17@users.noreply.github.com>
2024-09-24 23:31:23 +0530

    Update main.c

--- addressbookproject1/main.c
+++ addressbookproject1/main.c
@@ -1,5 +1,5 @@
 /**********************************************************************************************************************************
- * 												    Address book (miniproject 1)
+                                                    Address book (miniproject 1)
  ***********************************************************************************************************************************/
 /*
 Name        :-  Aditya shivaji kumbhar
@@ -18,16 +18,20 @@ struct data{
 };
 void add_contacts();
 void copy(FILE *,FILE *);
+int mobile_testcase(char *);
+int mail_testcase(char *);
+int name_testcase(char *);
 void search();
 void print();
 void edit();
 void delete();
+
 int main(){
 	char ch='y';
 	int choice;
 	while (ch=='y' || ch=='Y')
     {
-		printf(" 1.Adds\n 2.search\n 3.print\n 4.edit\n 5.delete\n Enter your choice: ");
+		printf(" \n1.Adds\n 2.search\n 3.print\n 4.edit\n 5.delete\n Enter your choice: ");
 		scanf("%d",&choice);
 		switch(choice){
 			case 1:
@@ -55,7 +59,9 @@ int main(){
     
 
 	}
+    
 }
+//function for adding contacts...
 void add_contacts()
 {
 	struct data contacts;
@@ -80,8 +86,8 @@ void add_contacts()
 
 
     printf("********************************************\n");
-	printf("\t   Enter your details here\t");
-	printf("\n********************************************\n");
+    printf("\t   Enter your details here\t");
+    printf("\n********************************************\n");
     //validation for the Name:
 
 
@@ -229,7 +235,7 @@ void add_contacts()
 	fclose(ftemp);
 }
 
-
+//function for searching...
 void search()
 {
 	int count = 0;
@@ -299,7 +305,7 @@ void search()
 	else if (match_count == 1)
 	 {
 		// If only one match is found, print the details
-                printf("********************************************\n");
+        printf("********************************************\n");
 		printf("\t\tContact\t");
 		printf("\n*********************************************\n");
 		printf("Name ID  : %s\n", matched_contacts[0].name);
@@ -330,8 +336,8 @@ void search()
 				if (strcmp(matched_contacts[j].mobile, mobile_i) == 0)      // if the mobile number is match with any mobile number with our same name contacts then print tahat index 
 				{
 					printf("********************************************\n");
-				        printf("\t\tContact no %d\t",j);
-				        printf("\n*********************************************\n");
+				    printf("\t\tContact no %d\t",j);
+				    printf("\n*********************************************\n");
 					printf("Name ID  : %s\n", matched_contacts[j].name);
 					printf("Mobile   : %s\n", matched_contacts[j].mobile);
 					printf("Email ID : %s\n", matched_contacts[j].email);
@@ -351,7 +357,7 @@ void search()
 				if (strcmp(matched_contacts[j].email, email_i) == 0)    //if any email address is matched with our email address then print that data.
 				{
 
-                                    printf("********************************************\n");
+                    printf("********************************************\n");
 				    printf("\t\tContact no %d\t",j);
 				    printf("\n*********************************************\n");
 					printf("Name ID   : %s\n", matched_contacts[j].name);
@@ -381,7 +387,6 @@ void search()
 
 }
 
-
 // function fpr printing the all data
 void print()
 {
@@ -397,187 +402,187 @@ void print()
 		fscanf(fdata, "%[^,],%[^,],%[^,],%[^\n]\n", contacts.name, contacts.mobile, contacts.email, contacts.address);
 		printf("********************************************\n");
 		printf("\t\tContact no %d\t",i);
-		printf("\n*********************************************\n");
+		printf("\n********************************************\n");
 		printf("Name ID  : %s\n", contacts.name);
 		printf("Mobile   : %s\n", contacts.mobile);
 		printf("Email ID : %s\n", contacts.email);
 		printf("Address  : %s\n\n", contacts.address);
+		printf("********************************************\n\n");
 	}
 	fclose(fdata);
 }
 
-
-//function for edit data
 void edit()
 {
+	
 	int count = 0;
 	struct data contacts;
-	FILE *fdata = fopen("data.csv","r");       //open file for reading
+	FILE *fdata = fopen("data.csv","r");       //open data file for the reading mode..
 	if(fdata == NULL)
 	{
-		printf("file not found");
+		printf("No data found\n");
+		return;  
 	}
-	FILE *ftemp = fopen("temp.csv","w");      //oen file for writing..
+
+	FILE *ftemp = fopen("temp.csv","w");     //open temp file for the writing...
 	if(ftemp == NULL)
 	{
-		printf("Temp file is not found");
+		printf("Temp file is not found\n");
+		fclose(fdata);
+		return;
 	}
-	
+
 	fscanf(fdata,"%d\n",&count);
-	copy(ftemp,fdata);
-	rewind(fdata);
+	fprintf(ftemp,"%d\n",count);
+
 	int type;
-	printf("enter the option which you want to edit :\n 1.Name\n 2.Mobile_number\n 3.Gmail\n 4.Address\n Enter your choice: ");
+
+	char name_i[30];
+	printf("Enter Name ID to Edit : ");
+	scanf(" %[^\n]", name_i);
+	printf("enter the option which you want to edit :\n 1).Name\n 2).Mobile_number\n 3).Gmail\n 4).Address\n Enter your choice : ");
 	scanf("%d",&type);
-	char mobile_id[30];                          //buffer
-	printf("Enter Name ID to Edit: ");
-	scanf(" %[^\n]",mobile_id);                        //buffer for store the temp name...
-	int i = 1;
-	while(i <= count){
+
+	int i = 1, flag = 0, temp = 0;
+	char mobile_i[11];
+
+	while(i <= count)
+	{
 		fscanf(fdata,"%[^,],%[^,],%[^,],%[^\n]\n", contacts.name, contacts.mobile, contacts.email, contacts.address);
-		if(strcmp(contacts.mobile,mobile_id) == 0)
+		if(strcmp(contacts.name,name_i) == 0)
 		{
-			switch(type)
+			flag++;
+		}
+		i++;
+	}
+
+	fseek(fdata, 0, SEEK_SET);            //set the offset to startiing.
+	fscanf(fdata, "%d\n", &count); 
+
+	if(flag == 1)
+	{
+		i = 1; 
+		while(i <= count)
+		{
+			fscanf(fdata,"%[^,],%[^,],%[^,],%[^\n]\n", contacts.name, contacts.mobile, contacts.email, contacts.address);
+			if(strcmp(contacts.name, name_i) == 0)
 			{
-				//case1 for the new name...
-				case 1:
-					printf("Enter new Name : ");
-					label6:
-					scanf("%[^\n]",contacts.name);
-					int i=0;
-					while(contacts.name[i]!='\0')
-					{
-						if((contacts.name[i]>='A'&& contacts.name[i]<='Z')||(contacts.name[i]>='a'&& contacts.name[i]<='z')||contacts.name[i]==' ')
-						{
-			
-						i++;
-						}
-						else
-						{
-						printf("Re-Enter the valid name : ");
-						goto label6;
-						}
-	                }
-                    break;
-
-                //case2 for the new mobile number...
-				case 2:
-					printf("Enter new Mobile: ");
-					label7:
-					rewind(fdata);                                   // Reset file pointer 
-    				fscanf(fdata, "%d\n", &count); 
-					scanf(" %[^\n]",contacts.mobile);
-					if(!(strlen(contacts.mobile)==10))              //here we check the length of our contact is valid or not
-					{
-						printf("length of your mobaile number is not match with the standerd length Re-enter: ");
-						goto label7;
-					}
-					for(int i=0;i<strlen(contacts.mobile);i++)
-					{
-						if(!(contacts.mobile[i]>='0' && contacts.mobile[i]<='9'))      //here we check all enterd inputs is number or not
-						{
-							printf("you enterd wrong thing re-enterd number: ");
-							goto label7;
-						}
-						else if((contacts.mobile[i]>='A'&& contacts.mobile[i]<='Z')||(contacts.mobile[i]>='a'&& contacts.mobile[i]<='z'))    //if any alphabate character is comming in the number then reenter the number.
-						{
-							goto label7;
-						}
-					}
-
-					//check the number is alrady in the file or not.
-					 for(i = 0; i < count; i++)
-    				{
-       					 struct data temp;                      //here we take the new structure variable to store the all data.
-       					 fscanf(fdata, "%[^,],%[^,],%[^,],%[^\n]\n", temp.name, temp.mobile, temp.email, temp.address);    // here we scan the all data from our data.csv file
-       					  if(strcmp(temp.mobile, contacts.mobile) == 0)                                     //here we check the our our scaned mobaile number is present or not in the data base and we accsess that using the temp variable
-       					  {
-           					 printf("Mobile number already exists enter a different mobile number: ");
-           					 goto label7;
-       					  }
-   				 	}
-					break;
-
-                //case3 for the email address..
-				case 3:
-					printf("Enter new Email ID: ");
-					int k=0;
-					label4 :
-					rewind(fdata);                               // Reset file pointer 
-					fscanf(fdata, "%d\n", &count);              // Read the count again
-					scanf(" %[^\n]",contacts.email);
-
-					int m=0;
-					while(!m)
-					{
-						
-						if(strlen(contacts.email)>10 && strcmp(contacts.email+strlen(contacts.email)-10,"@gmail.com")==0 && (contacts.email[k]>='a'&& contacts.email[k]<='z'))
-						/*the above line explanation first it check the our enterd emaillength is greater than 10 or not 
-						then check at the last all the prdifined character is present or not in the email 
-						then it check the all the character we enterd in the email is small or not if one of the condition get false then reenter the email address*/
-						{
-							k++;
-							m=1;
-						}
-						else
-						{
-							printf("Invalid email!!! Re-enter the email : ");
+				switch(type) {
+					case 1:
+						printf("Enter new Name : ");
+                        label4:
+						scanf(" %[^\n]", contacts.name);
+						if(name_testcase(contacts.name))
 							goto label4;
-						}
-					}
+						break;
 
-					// Check if email already exists
-					for(i = 0; i < count; i++)
-					{
-						struct data temp;
-						fscanf(fdata, "%[^,],%[^,],%[^,],%[^\n]\n", temp.name, temp.mobile, temp.email, temp.address);
-						if(strcmp(temp.email, contacts.email) == 0)
-						{
-							printf("Email address already exists. Enter a different email: ");
-							goto label4;
-						}
-					}
-					break;
 
-				//case4 for the address...
+					case 2:
+						printf("Enter new Mobile : ");
+                        label5:
+						scanf(" %[^\n]", contacts.mobile);
+						if(mobile_testcase(contacts.mobile))
+							goto label5;
+						break;
 
-				case 4:
-					printf("Enter new Address: ");
-					label9:
-					scanf(" %[^\n]",contacts.address);
-					int j=0;
-					while(contacts.address[j]!='\0')
-					{
-						if((contacts.address[j]>='A'&& contacts.address[j]<='Z')||(contacts.address[j]>='a'&& contacts.address[j]<='z')||contacts.address[j]==' ')
-						{
-			
-							j++;
-						}
-						else
-						{
-							printf("Re-Enter the valid address : ");
-							goto label9;
-						}
 
-					}
+					case 3:
+						printf("Enter new Email ID : ");
+                        label6:
+						scanf(" %[^\n]", contacts.email);
+						if(mail_testcase(contacts.email))
+							goto label6;
+						break;
 
-					break;
-				    
-					
+
+					case 4:
+						printf("Enter new Address : ");
+						scanf(" %[^\n]", contacts.address);
+						break;
 					default:
-					printf("Invalid option\n");
-					break;
+						printf("Invalid option\n");
+						break;
+				}
 			}
+			fprintf(ftemp, "%s,%s,%s,%s\n", contacts.name, contacts.mobile, contacts.email, contacts.address);
+			i++;
+		}
+	}
+
+	if(flag > 1)
+	{
+		printf("Multiple names found.\nPlease enter mobile number to compare : ");
+		scanf(" %[^\n]", mobile_i);
+
+		i = 1;  
+		while(i <= count)
+		{
+			fscanf(fdata, "%[^,],%[^,],%[^,],%[^\n]\n", contacts.name, contacts.mobile, contacts.email, contacts.address);
+			if(strcmp(contacts.name, name_i) == 0 && strcmp(contacts.mobile, mobile_i) == 0)
+			{
+				temp++;
+				switch(type) {
+					case 1:
+						printf("Enter new Name : ");
+                                                labela:
+						scanf(" %[^\n]", contacts.name);
+						if(name_testcase(contacts.name))
+							goto labela;
+						break;
 
 
+					case 2:
+						printf("Enter new Mobile : ");
+                                                labelb:
+						scanf(" %[^\n]", contacts.mobile);
+						if(mobile_testcase(contacts.mobile))
+							goto labelb;
+						break;
+
+
+					case 3:
+						printf("Enter new Email ID : ");
+                                                labelc:
+						scanf(" %[^\n]", contacts.email);
+						if(mail_testcase(contacts.email))
+							goto labelc;
+						break;
+
+
+					case 4:
+						printf("Enter new Address : ");
+						scanf(" %[^\n]", contacts.address);
+						break;
+
+
+					default:
+						printf("Invalid option\n");
+						break;
+				}
+			}
+			fprintf(ftemp, "%s,%s,%s,%s\n", contacts.name, contacts.mobile, contacts.email, contacts.address);
+			i++;
 		}
 
-	}	
+		if(temp == 0)
+			printf("Mobile number not found\n");
+	}
+
 	fclose(fdata);
 	fclose(ftemp);
-	if(i == count+1)
-		printf("Name not found");
+
+	fdata = fopen("data.csv","w");
+	ftemp = fopen("temp.csv","r");
+	copy(fdata, ftemp);
+	fclose(fdata);
+	fclose(ftemp);
+
+	if(flag == 0)
+		printf("Name not found\n");
+	else
+		printf("Data edited successfully!!\n");
 }
+
 //function for delet the  the contact details..
 void delete()
 {
@@ -625,12 +630,111 @@ void delete()
 		printf("Name not found\n");
 
 }
+
 // function for the copy fro one file to another file.
-void copy(FILE *fdata,FILE *ftemp){
+void copy(FILE *fdata,FILE *ftemp)
+{
 	char ch;
 	while((ch=fgetc(ftemp))!=EOF)
 	{
 		fputc(ch,fdata);
+	}
+}
+
+//validation fro the mobile
+int mobile_testcase(char *mobile)
+{
+
+	if(strlen(mobile)!=10)
+	{
+		printf("Enter the number that has 10 digits : ");
+		return 1;
+	}
+
+	int i=0;
+	while(mobile[i])
+	{
+		if(isdigit(mobile[i]))
+		{
+			i++;
+		}
+		else
+		{
+			printf("Enter only digits : ");
+			return 1;
+		}
+	}
+
+
+	struct data detail;
+
+	int count=0;
+	FILE *fdata=fopen("data.csv","r");
+
+	if( fdata == NULL)
+	{
+		printf("File not openend\n");
+		return 0;
+	}
+	fscanf(fdata,"%d\n",&count);
+
+	for(int i=0;i<count;i++)
+	{
+		fscanf(fdata,"%[^,],%[^,],%[^,],%[^\n]\n",detail.name, detail.mobile, detail.email, detail.address);
+		if(strcmp(detail.mobile,mobile)==0)
+		{
+			printf("Enter new number : ");
+			return 1;
+		}
+	}
+	fclose(fdata);
+}
+
+
+//validation for the name.
+int name_testcase(char *name)
+{
+	int i = 0;
+	while(name[i])
+	{
+		if(isalpha(name[i]) || name[i] == ' ')
+			i++;
+		else{
+			printf("Enter the name that has alphabets : \n");
+			return 1;
+		}
+	}
+}
+
+//mail test case..
+int mail_testcase(char *mail)
+{
+	char *temp;
+	if(temp=strstr(mail,"@gmail.com"))
+	{
+		if(temp[10]=='\0')
+		{
+			int i=0;
+			while(mail[i])
+			{
+				if(mail[i]==' ' || mail[i]=='.' && mail[i+1]=='@' || mail[i]=='a' && mail[i+1]=='.')
+				{
+					printf("Enter the mail correctly : ");
+					return 1;
+				}
+				i++;
+			}
+		}
+		else
+		{
+			printf("Enter the mail that ends with \"@gmail.com\" : ");
+			return 1;
+		}
+	}
+	else
+	{
+		printf("Enter the mail that contain \"@gmail.com\" : ");
+		return 1;
 	}
 }
 
